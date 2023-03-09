fcd74488cae81b239b0313b3fc77496235056183
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-03-09 22:38:11 +0530

    changes are added

--- src/components/Fonts.js
+++
@@ -1,7 +0,0 @@
-import { Font } from "expo-font";
-
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    Montserrat: require("../../assets/fonts/Montserrat-Medium.ttf"),
-  });
-};
